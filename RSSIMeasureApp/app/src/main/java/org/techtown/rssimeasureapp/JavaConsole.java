9c0600e4e9b4398a6ebed3d4f0c2359fb42477af
Park Sung Hyun <ayanoquasar@gmail.com>
2022-04-21 08:08:21 +0900

    added code for console testing

---
+++ RSSIMeasureApp/app/src/main/java/org/techtown/rssimeasureapp/JavaConsole.java
@@ -0,0 +1,9 @@
+package org.techtown.rssimeasureapp;
+
+public class JavaConsole {
+    public static void main(String[] args){
+        System.out.println("Hello, world!");
+        Triangulation loc = new Triangulation(0.76, 0.99);
+        System.out.println(loc.x + ", " + loc.y);
+    }
+}
