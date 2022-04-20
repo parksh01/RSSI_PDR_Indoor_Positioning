0ec0225bd2e859ccd42ffb7444a642edac634990
Park Sung Hyun <ayanoquasar@gmail.com>
2022-04-21 07:46:10 +0900

    Revert "Beacon code refactored and added 2nd beacon"
    This reverts commit 364a110b267251baa072d713e579ab9c7a33c793.

--- Bluetooth Beacon (Arduino)/btbeacon/btbeacon.ino
+++ Bluetooth Beacon (Arduino)/btbeacon/btbeacon.ino
@@ -1,9 +1,6 @@
 #include <SoftwareSerial.h>
 
-static int TX = 4;
-static int RX = 5;
-
-SoftwareSerial BTSerial(TX, RX); // TX/RX
+SoftwareSerial BTSerial(4, 5); // TX/RX
 void setup(){
   Serial.begin(9600);
   Serial.println("Hello!");
