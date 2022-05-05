b103afb62c60462a56c801d6af06ce36d2ebc5df
parksh01 <101562734+parksh01@users.noreply.github.com>
2022-05-05 19:33:02 +0900

    Applied Curve Fitting on Kalman Filter-Applied RSSI Values

--- RSSICurveFitting/main.py
+++ RSSICurveFitting/main.py
@@ -2,17 +2,26 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit
 
+def most_frequent(data):
+    return max(data, key=data.count)
+
 front_or_back = input('front or back? : ')
+kalman_or_not = input('kalman? (y/n) : ')
 
 # values of rssi, will be used for curve fitting.
 rssi_y = []
 rssi_avg = []
+rssi_median = []
+rssi_max = []
 
 filename = 'inputdata'
 if front_or_back == 'front':
     filename += '/front/'
 elif front_or_back == 'back':
     filename += '/back/'
+
+if kalman_or_not == 'y':
+    filename += 'kf/'
 
 # read files and get rssi values
 for i in range(7):
@@ -29,22 +38,35 @@ for i in range(7):
             tick += 1
         avg /= float(tick)
         rssi_y.append(rssiCurrentValues)
+        rssi_median.append(np.median(rssiCurrentValues))
+        rssi_max.append(most_frequent(rssiCurrentValues))
         rssi_avg.append(avg)
 
 
 # Curve Fitting
-def func(X, n, A): # a = n / b = A
+def func(X, n, A):
     # distance to rssi
     return -10 * n * np.log10(X) + A
 xvalues = np.arange(1, 7+1)
-popt, pcov = curve_fit(func, xvalues, rssi_avg, method='lm')
-print(popt)
+popt1, pcov1 = curve_fit(func, xvalues, rssi_avg, method='lm')
+popt2, pcov2 = curve_fit(func, xvalues, rssi_median, method='lm')
+popt3, pcov3 = curve_fit(func, xvalues, rssi_max, method='lm')
+
+print('A =', rssi_avg[0])
+print('avg :', popt1)
+print('median :', popt2)
+print('most :', popt3)
 
 # show the graph of measured rssi values and fitted curve
 fig, ax = plt.subplots()
 violin = ax.violinplot(rssi_y, positions=range(1, 7 + 1))
 ax.set_xlabel('meters')
 ax.set_ylabel('RSSI')
-plt.plot(range(1, 7 + 1), rssi_avg, 'r^')
-plt.plot(xvalues, func(xvalues, *popt))
+plt.plot(range(1, 7 + 1), rssi_avg, 'r^', label='rssi_avg')
+plt.plot(range(1, 7 + 1), rssi_median, 'bo', label='rssi_median')
+plt.plot(range(1, 7 + 1), rssi_max, 'gs', label='rssi_most')
+plt.plot(xvalues, func(xvalues, *popt1), label='curve(avg)')
+plt.plot(xvalues, func(xvalues, *popt2), label='curve(median)')
+plt.plot(xvalues, func(xvalues, *popt3), label='curve(most)')
+plt.legend()
 plt.show()
--- RSSIMeasureApp/app/src/main/java/org/techtown/rssimeasureapp/JavaConsole.java
+++ RSSIMeasureApp/app/src/main/java/org/techtown/rssimeasureapp/JavaConsole.java
@@ -1,9 +1,34 @@
 package org.techtown.rssimeasureapp;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class JavaConsole {
-    public static void main(String[] args){
-        System.out.println("Hello, world!");
-        Triangulation loc = new Triangulation(0.76, 0.99);
-        System.out.println(loc.x + ", " + loc.y);
+    public static void main(String[] args) throws IOException {
+        String fileLocation = "C:\\ParkingLot\\ParkingLot\\RSSICurveFitting\\inputdata\\front\\";
+        for(int i = 1;i<=7;i++){
+            KalmanFilter kf = new KalmanFilter();
+            String fileName = "" + i + "m front.csv";
+            System.out.println(fileLocation + fileName);
+            BufferedReader reader = new BufferedReader(
+                    new FileReader(fileLocation + fileName)
+            );
+
+            String fileNameOutput = "kf\\" + i + "m front(kalman).csv";
+            BufferedWriter writer = new BufferedWriter(
+                    new FileWriter(fileLocation + fileNameOutput)
+            );
+            String str;
+            while ((str = reader.readLine()) != null) {
+                System.out.println(kf.filtering(Integer.parseInt(str)));
+                writer.write("" + kf.filtering(Integer.parseInt(str)) + '\n');
+            }
+            kf = null;
+            reader.close();
+            writer.close();
+        }
     }
 }
