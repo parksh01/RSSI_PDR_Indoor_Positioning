9a550dcf67a3720f0e1f3bf8189b28f4c6885819
parksh01 <101562734+parksh01@users.noreply.github.com>
2022-05-06 21:43:01 +0900

    Recomputed Curve Fitting

--- RSSICurveFitting/main.py
+++ RSSICurveFitting/main.py
@@ -5,88 +5,89 @@ from scipy.optimize import curve_fit
 def most_frequent(data):
     return max(data, key=data.count)
 
-front_or_back = input('front or back? : ')
-kalman_or_not = input('kalman? (y/n) : ')
-
 # values of rssi, will be used for curve fitting.
-rssi_y = []
-rssi_avg = []
-rssi_median = []
-rssi_max = []
+rssi_y_raw = []
+rssi_y_kf = []
 
-filename = 'inputdata'
-if front_or_back == 'front':
-    filename += '/front/'
-elif front_or_back == 'back':
-    filename += '/back/'
+rssi_avg_raw = []
+rssi_avg_kf = []
 
-if kalman_or_not == 'y':
-    filename += 'kf/'
+front_or_back = input("front or back? : ")
+meters = int(input("meters? : "))
+kf_or_raw = input("kf or raw? : ")
+
+filename = 'inputdata2/'
 
 # read files and get rssi values
 for i in range(7):
-    with open(filename + str(i + 1) + 'm ' + front_or_back + '.csv', 'r') as f:
+    with open(filename + str(i + 1) + ' ' + front_or_back + '.csv', 'r') as f:
         print('Processing : ' + filename + str(i + 1) + 'm ' + front_or_back + '.csv')
         line = None
         avg = 0
         tick = 0
-        rssiCurrentValues = []
+        rssiCurrentValuesRaw = []
+        rssiCurrentValuesKF = []
         j = 0
         while True:
             line = f.readline().strip()
             if line == '':
                 break
-            rssiCurrentValues.append(float(line))
-            avg += float(line)
+            rssiCurrentValuesRaw.append(float(line.split(',')[0]))
+            rssiCurrentValuesKF.append(float(line.split(',')[1]))
             tick += 1
-        avg /= float(tick)
-        rssi_y.append(rssiCurrentValues)
-        rssi_median.append(np.median(rssiCurrentValues))
-        rssi_max.append(most_frequent(rssiCurrentValues))
-        rssi_avg.append(avg)
-
+        rssi_y_raw.append(rssiCurrentValuesRaw)
+        rssi_y_kf.append(rssiCurrentValuesKF)
+        rssi_avg_raw.append(round(np.average(rssiCurrentValuesRaw), 3))
+        rssi_avg_kf.append(round(np.average(rssiCurrentValuesKF), 3))
 
 # Curve Fitting
 def func(X, n, A):
     # distance to rssi
     return -10 * n * np.log10(X) + A
 
-def func2(X, n):
-    return -10 * n * np.log10(X) + rssi_avg[0]
+xvalues = np.arange(1, meters + 1)
+popt1, pcov1 = curve_fit(func, xvalues, rssi_avg_raw[:meters], method='lm')
+popt2, pcov2 = curve_fit(func, xvalues, rssi_avg_kf[:meters], method='lm')
 
+print('raw :', popt1)
+print('kf :', popt2)
+print('avg_raw :', rssi_avg_raw)
+print('avg_kf :', rssi_avg_kf)
 
-xvalues = np.arange(1, 7+1)
-popt1, pcov1 = curve_fit(func, xvalues, rssi_avg, method='lm')
-popt2, pcov2 = curve_fit(func, xvalues, rssi_median, method='lm')
-popt3, pcov3 = curve_fit(func, xvalues, rssi_max, method='lm')
-
-print('avg :', popt1)
-print('median :', popt2)
-print('most :', popt3)
-print(rssi_avg)
-rssi_to_dist = []
+rssi_to_dist_raw = []
+rssi_to_dist_raw_error = []
 for i in range(7):
-      rssi_to_dist.append(10 ** ((popt1[1] - rssi_avg[i])/(10*popt1[0])))
-    # rssi_to_dist.append(10 ** ((rssi_avg[0] - rssi_avg[i])/(10*popt1[0])))
-print(rssi_to_dist)
+      rssi_to_dist_raw.append(round(10 ** ((popt1[1] - rssi_avg_raw[i])/(10*popt1[0])), 3))
+      rssi_to_dist_raw_error.append(np.absolute(round(float(i + 1) - rssi_to_dist_raw[i], 3)))
+print('dist(raw) :', rssi_to_dist_raw)
+print('error(raw) :', rssi_to_dist_raw_error)
+print('mean error(raw) :', np.mean(rssi_to_dist_raw_error))
+print('error std(raw) :', np.std(rssi_to_dist_raw_error))
+print()
 
-print('1.5m :', 10 ** ((popt1[1] + 61.5)/(10 * popt1[0])))
-print('0.5m :', 10 ** ((popt1[1] + 50.5333)/(10 * popt1[0])))
+rssi_to_dist_kf = []
+rssi_to_dist_kf_error = []
+for i in range(7):
+      rssi_to_dist_kf.append(round(10 ** ((popt2[1] - rssi_avg_kf[i])/(10*popt2[0])), 3))
+      rssi_to_dist_kf_error.append(np.absolute(round(float(i + 1) - rssi_to_dist_kf[i], 3)))
+print('dist(kf) :', rssi_to_dist_kf)
+print('error(kf) :', rssi_to_dist_kf_error)
+print('mean error(kf) :', np.mean(rssi_to_dist_kf_error))
+print('error std(kf) :', np.std(rssi_to_dist_kf_error))
 
 # show the graph of measured rssi values and fitted curve
 fig, ax = plt.subplots()
-violin = ax.violinplot(rssi_y, positions=range(1, 7 + 1))
 ax.set_xlabel('meters')
 ax.set_ylabel('RSSI')
-plt.plot(range(1, 7 + 1), rssi_avg, 'r^', label='rssi_avg')
-"""
-plt.plot(range(1, 7 + 1), rssi_median, 'bo', label='rssi_median')
-plt.plot(range(1, 7 + 1), rssi_max, 'gs', label='rssi_most')
-"""
-plt.plot(xvalues, func(xvalues, *popt1), label='curve(avg)', color='r')
-"""
-plt.plot(xvalues, func(xvalues, *popt2), label='curve(median)', color='b')
-plt.plot(xvalues, func(xvalues, *popt3), label='curve(most)', color='g')
-"""
+
+if kf_or_raw == 'kf':
+    violin = ax.violinplot(rssi_y_kf, positions=range(1, 7 + 1))
+    plt.plot(range(1, 7 + 1), rssi_avg_kf, 'r^', label='rssi_avg_kf')
+    plt.plot(range(1, 7 + 1), func(range(1, 7 + 1), *popt2), label='curve(avg)', color='r')
+elif kf_or_raw == 'raw':
+    violin = ax.violinplot(rssi_y_raw, positions=range(1, 7 + 1))
+    plt.plot(range(1, 7 + 1), rssi_avg_raw, 'r^', label='rssi_avg_raw')
+    plt.plot(range(1, 7 + 1), func(range(1, 7 + 1), *popt1), label='curve(avg)', color='r')
+
 plt.legend()
 plt.show()
