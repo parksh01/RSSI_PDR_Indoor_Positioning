90f6c2828a74ea064556d334050efa38b28d0c1a
parksh01 <101562734+parksh01@users.noreply.github.com>
2022-07-28 15:12:19 +0900

    implemented 4-point trilateration on python

--- RSSILocation/RSSILocation_RSSIvalue.py
+++ RSSILocation/RSSILocation_RSSIvalue.py
@@ -1,6 +1,22 @@
 # RSSILocation Refactored
 import numpy as np
 
+class Beacon:
+    def __init__(self, addr, A_front, n_front, A_back, n_back, isBack, x, y):
+        self.addr = addr
+        self.A_front = A_front
+        self.n_front = n_front
+        self.A_back = A_back
+        self.n_back = n_back
+        self.isBack = isBack
+        self.x = x
+        self.y = y
+    def getDist(self, rssi):
+        if not self.isBack:
+            return 10**((rssi - self.A_front)/(-10 * self.n_front))
+        elif self.isBack:
+            return 10**((rssi - self.A_back)/(-10 * self.n_back))
+
 def printError(distErrorX, distErrorY, keyword):
     print('error(' + keyword + ') : ' + str(np.average(distErrorX)) + ',' + str(np.average(distErrorY)))
     print('std(' + keyword + ') : ' + str(np.std(distErrorX)) + ',' + str(np.std(distErrorY)))
@@ -8,38 +24,36 @@ def printError(distErrorX, distErrorY, keyword):
 def errorRatio(x1, x2, xerror1, xerror2):
     return (x1*xerror2 + x2*xerror1)/(xerror1+xerror2)
 
-# pre defined variables area
+# pre defined variables (beacon info and log file locations)
 foldername = 'Measure20220518'
 filename = 'Coord'
-coord = 4
-beacons = ['A81B6AAE5FF6', 'A81B6AAE4C6B', 'A81B6AAE5260']
-beacon_A_front = [-64.44, -59.717, -58.582]
-beacon_n_front = [1.569, 1.769, 1.837]
-beacon_A_back = [-66.028, -61.764, -60.356]
-beacon_n_back = [1.071, 0.967, 1.709]
+beacons = []
+beacons.append(Beacon('A81B6AAE5FF6', -64.44, 1.569, -66.028, 1.071, False, x = 0.0, y = 0.0))
+beacons.append(Beacon('A81B6AAE4C6B', -59.717, 1.769, -61.764, 0.967, False, x = 4.0, y = 0.0))
+beacons.append(Beacon('A81B6AAE5260', -58.582, 1.837, -60.356, 1.709, True, x = 0.0, y = 4.0))
+coord = [beacons[1].x, beacons[2].y]
 
 # start of algorithm
+measureType = str()
+if len(beacons) == 3:
+    measureType = input("type? 1 / 2 : ")
 beaconRSSI = []
 beaconDist = []
 distErrorX = []
 distErrorY = []
-distErrorX1 = []
-distErrorY1 = []
-distErrorX2 = []
-distErrorY2 = []
-# beacon01 : (0, 0)
-# beacon02 : (5, 0)
-# beacon03 : (0, 5)
-for i in range(1, coord):
-    for j in range(1, coord):
+for i in range(1, int(coord[1])):
+    for j in range(1, int(coord[0])):
         # processing coordinate (j, i)
         beaconDistTemp = []
         beaconNum = -1
         x, y = [], []
+
+        # First, get RSSI and distance info from each beacons.
         for beaconInfo in beacons:
             beaconNum += 1
-            f = open(foldername + '/' + filename + str(j) + str(i) + '/' + beaconInfo + '.csv', 'r')
+            f = open(foldername + '/' + filename + str(j) + str(i) + '/' + beaconInfo.addr + '.csv', 'r')
             while True:
+                # use Kalman filtered average values
                 line = f.readline()
                 processLine = line.split(',')
                 if not line:
@@ -47,33 +61,38 @@ for i in range(1, coord):
                 elif processLine[0] == 'avg(rssi)':
                     line = f.readline()
                     processLine = line.split(',')
-                    # use Kalman filtered values
                     beaconRSSI.append(float(processLine[1]))
-                    if beaconNum == 2:
-                        beaconDistTemp.append(round(10 ** ((float(processLine[1]) - beacon_A_back[beaconNum])/(-10 * beacon_n_back[beaconNum])), 3))
-                    else:
-                        beaconDistTemp.append(round(10 ** ((float(processLine[1]) - beacon_A_front[beaconNum])/(-10 * beacon_n_front[beaconNum])), 3))
+                    beaconDistTemp.append(beaconInfo.getDist(float(processLine[1])))
                     break
         beaconDist.append(beaconDistTemp)
-        """
-        x.append(((beaconDistTemp[0] ** 2) - (beaconDistTemp[1] ** 2) - (float(coord)**2)) / (-2 * float(coord)))
-        y.append(((beaconDistTemp[2] ** 2) - (beaconDistTemp[1] ** 2) - (float(coord)**2)) / (-2 * float(coord)))
-        x.append(abs(coord - (abs((beaconDistTemp[2]**2)-(x[0]**2))**0.5)))
-        y.append(abs(coord - (abs((beaconDistTemp[0]**2)-(y[0]**2))**0.5)))
-        """
-        x.append(((beaconDistTemp[1] ** 2) - (beaconDistTemp[0] ** 2) - (float(coord)**2)) / (-2 * float(coord)))
-        y.append(((beaconDistTemp[2] ** 2) - (beaconDistTemp[0] ** 2) - (float(coord)**2)) / (-2 * float(coord)))
-        x.append(abs(coord - (abs((beaconDistTemp[2]**2)-(x[0]**2))**0.5)))
-        y.append(abs(coord - (abs((beaconDistTemp[1]**2)-(y[0]**2))**0.5)))
+
+        # then calculate the coordinate.
+        if len(beacons) == 3:
+            if measureType == '1':
+                x.append(((beaconDistTemp[1] ** 2) - (beaconDistTemp[0] ** 2) - (float(coord[0]) ** 2)) / (-2 * float(coord[0])))
+                y.append(((beaconDistTemp[2] ** 2) - (beaconDistTemp[0] ** 2) - (float(coord[1]) ** 2)) / (-2 * float(coord[1])))
+                x.append(abs((beaconDistTemp[0] ** 2 - y[0] ** 2) ** 0.5))
+                y.append(abs((beaconDistTemp[0] ** 2 - x[0] ** 2) ** 0.5))
+            elif measureType == '2':
+                x.append(((beaconDistTemp[1] ** 2) - (beaconDistTemp[0] ** 2) - (float(coord[0]) ** 2)) / (-2 * float(coord[0])))
+                y.append(((beaconDistTemp[2] ** 2) - (beaconDistTemp[0] ** 2) - (float(coord[1]) ** 2)) / (-2 * float(coord[1])))
+                x.append(abs(coord[0] - (abs((beaconDistTemp[1] ** 2) - (y[0] ** 2)) ** 0.5)))
+                y.append(abs(coord[1] - (abs((beaconDistTemp[2] ** 2) - (x[0] ** 2)) ** 0.5)))
+        elif len(beacons) == 4:
+            x.append((beaconDistTemp[1]**2 - beaconDistTemp[0]**2 - float(coord[0])**2)/(-2 * float(coord[0])))
+            y.append(abs(beaconDistTemp[0]**2 - x[0]**2)**0.5)
+
+            y.append((beaconDistTemp[2]**2 - beaconDistTemp[0]**2 - float(coord[1])**2)/(-2 * float(coord[1])))
+            x.append(abs(beaconDistTemp[0]**2 - y[1]**2)**0.5)
+
+            x.append((beaconDistTemp[3]**2 - beaconDistTemp[2]**2 - float(coord[0])**2)/(-2 * float(coord[0])))
+            y.append(abs(beaconDistTemp[2]**2 - x[2]**2)**0.5 + float(coord[1]))
+
+            y.append((beaconDistTemp[3]**2 - beaconDistTemp[1]**2 - float(coord[1]))/(-2 * float(coord[1])))
+            x.append(abs(beaconDistTemp[1]**2 - y[3]**2)**0.5 + float(coord[0]))
         
         xcoord, ycoord = np.average(x), np.average(y)
         print('(' + str(j) + ', ' + str(i) + ') : ' + str(round(xcoord, 3)) + ',' +  str(round(ycoord, 3)) + '/ error : ' + str(round(abs(j-round(xcoord, 3)), 3)) + ',' + str(round(abs(i-round(ycoord, 3)), 3)))
         distErrorX.append(round(abs(j-round(xcoord, 3)), 3))
         distErrorY.append(round(abs(i-round(ycoord, 3)), 3))
-        distErrorX1.append(round(abs(j-round(x[0], 3)), 3))
-        distErrorY1.append(round(abs(i-round(y[0], 3)), 3))
-        distErrorX2.append(round(abs(j-round(x[1], 3)), 3))
-        distErrorY2.append(round(abs(i-round(y[1], 3)), 3))
 printError(distErrorX, distErrorY, 'avg')
-printError(distErrorX1, distErrorY1, '1')
-printError(distErrorX2, distErrorY2, '2')
